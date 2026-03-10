a526cd0365ed4378811ab7a3a8eb7ba384bc48b6
tutfruto <joaopedroluiz76@gmail.com>
2026-03-10 15:03:54 -0300

    Index.js

--- Index.js
+++ Index.js
@@ -1,22 +1,52 @@
 const mineflayer = require('mineflayer')
 
-function criarBot() {
+function startBot() {
+
   const bot = mineflayer.createBot({
     host: 'Sonsofthemine.aternos.me',
-    port: http,
-    username: 'Bot24h'
-    version: 1.21.4
+    port: 49135,
+    username: 'Bot24h',
+    version: '1.21.4'
   })
 
   bot.on('spawn', () => {
     console.log('Bot entrou no servidor!')
+
+    // pular a cada 30 segundos
+    setInterval(() => {
+      bot.setControlState('jump', true)
+
+      setTimeout(() => {
+        bot.setControlState('jump', false)
+      }, 500)
+
+    }, 30000)
+
+    // andar um pouco
+    setInterval(() => {
+
+      const actions = ['forward', 'back', 'left', 'right']
+      const action = actions[Math.floor(Math.random() * actions.length)]
+
+      bot.setControlState(action, true)
+
+      setTimeout(() => {
+        bot.setControlState(action, false)
+      }, 2000)
+
+    }, 60000)
+
   })
 
   bot.on('end', () => {
-    setTimeout(criarBot, 5000)
+    console.log('Bot desconectou, tentando reconectar...')
+    setTimeout(startBot, 5000)
   })
 
-  bot.on('error', console.log)
+  bot.on('error', (err) => {
+    console.log('Erro:', err)
+  })
+
 }
 
-criarBot()
+startBot()
